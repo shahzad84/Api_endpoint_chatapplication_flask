5a6249d0b086878d57e7562d62b341039e255cf9
hilarious3 <shahzad20022002@gmail.com>
2023-09-23 17:13:41 +0530

    connecting to sql database

--- model/user_model.py
+++ model/user_model.py
@@ -1,3 +1,10 @@
+import mysql.connector
 class user_model():
+    def __init__(self):
+        try:
+            con=mysql.connector.connect(host="localhost",user="root",password="password",database="chatflask",port=1080)
+            print("connected_________!")
+        except:
+            print("__some error")
     def login(self):
         return "hii this is model"
