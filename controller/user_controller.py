2aa334d1bdcdb5814cbff794bbe7a5bddd61321f
hilarious3 <shahzad20022002@gmail.com>
2023-09-23 17:55:48 +0530

    initial get commit

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,4 +1,5 @@
 from app import app
+from flask import request
 from model.user_model import user_model
 obj=user_model()
 @app.route("/login")
--- model/user_model.py
+++ model/user_model.py
@@ -1,10 +1,19 @@
 import mysql.connector
+import json
+from flask import make_response
 class user_model():
     def __init__(self):
         try:
-            con=mysql.connector.connect(host="localhost",user="root",password="password",database="chatflask",port=1080)
+            self.con=mysql.connector.connect(host="localhost",user="root",password="password",database="chatflask",port=1080)
+            self.con.autocommit=True
+            self.cur=self.con.cursor(dictionary=True)
             print("connected_________!")
         except:
             print("__some error")
     def login(self):
-        return "hii this is model"
+        self.cur.execute("SELECT* FROM chat")
+        result=self.cur.fetchall()
+        if len(result)>0:
+            return make_response({"payload":result},200)
+        else:
+            return make_response({"message":"No Data Found"},204)
